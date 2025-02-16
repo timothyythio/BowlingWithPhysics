22a402f657312e2905bd308722c093dd770c9ecd
timothyythio <63795701+timothyythio@users.noreply.github.com>
2025-02-15 21:19:42 -0800

    Ball moves with player

--- Assets/BallController.cs
+++ Assets/BallController.cs
@@ -5,11 +5,17 @@ public class BallController : MonoBehaviour
 {
     [SerializeField] private float force = 1f;
     [SerializeField] private InputManager inputManager;
+    [SerializeField] private Transform ballAnchor;
+
+    private bool isBallLaunched;
     private Rigidbody ballRB;
     void Start()
     {
         ballRB = GetComponent<Rigidbody>();
         inputManager.OnSpacePressed.AddListener(LaunchBall);
+        transform.parent = ballAnchor;
+        transform.localPosition = Vector3.zero;
+        ballRB.isKinematic = true;
     }
 
     void Update()
@@ -19,6 +25,10 @@ public class BallController : MonoBehaviour
 
     private void LaunchBall()
     {
+        if (isBallLaunched) return;
+        isBallLaunched = true;
+        transform.parent = null;
+        ballRB.isKinematic = false;
         ballRB.AddForce(transform.forward * force, ForceMode.Impulse);
     }
 }
