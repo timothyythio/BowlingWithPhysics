eac5b36a5780c27e2e60b510fb3c3ac63aeaf9b9
timothyythio <63795701+timothyythio@users.noreply.github.com>
2025-02-15 20:26:59 -0800

    Added gutter + script

---
+++ Assets/Gutter.cs
@@ -0,0 +1,29 @@
+using UnityEngine;
+
+public class Gutter : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    public void OnTriggerEnter(Collider triggeredBody)
+    {
+        //Store RigidBody of ball in ballRigidBody
+        Rigidbody ballRigidBody = triggeredBody.GetComponent<Rigidbody>();
+        //Store velocity magnitude before resetting the velocity
+        float velocityMagnitude = ballRigidBody.linearVelocity.magnitude;
+        //Reset linear and angular velocity because ball rotates on ground when moving
+        ballRigidBody.linearVelocity = Vector3.zero;
+        ballRigidBody.angularVelocity = Vector3.zero;
+        //Adds the force that propels the ball to the direction of the gutter
+        ballRigidBody.AddForce(transform.forward * velocityMagnitude, ForceMode.VelocityChange);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
