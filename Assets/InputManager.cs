cb897b62ba4b64df644b11d67b505d3d66dd76b5
timothyythio <63795701+timothyythio@users.noreply.github.com>
2025-02-15 21:12:13 -0800

    Added Player Movement

--- Assets/InputManager.cs
+++ Assets/InputManager.cs
@@ -4,6 +4,7 @@ using UnityEngine.Events;
 
 public class InputManager : MonoBehaviour
 {
+    public UnityEvent<Vector2> OnMove = new UnityEvent<Vector2>();
     public UnityEvent OnSpacePressed = new UnityEvent();
     void Start()
     {
@@ -14,6 +15,16 @@ public class InputManager : MonoBehaviour
     {
         if (Input.GetKeyDown(KeyCode.Space)) {
             OnSpacePressed.Invoke();
-        }    
+        }
+        Vector2 input = Vector2.zero;
+        if (Input.GetKeyUp(KeyCode.A))
+        {
+            input += Vector2.left;
+        }
+        if (Input.GetKeyUp(KeyCode.D))
+        {
+            input += Vector2.right;
+        }
+        OnMove?.Invoke(input);
     }
 }
